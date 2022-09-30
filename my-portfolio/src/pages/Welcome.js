d87f786105f85633345ad06e3c85ee7c0c7b430a
Jason Martinez <jasonmar08@gmail.com>
2022-09-30 15:25:31 -0400

    added built from scratch on welcome page

--- my-portfolio/src/pages/Welcome.js
+++ my-portfolio/src/pages/Welcome.js
@@ -15,9 +15,12 @@ const Welcome = () => {
         <h2 data-aos="flip-up">Full-Stack Software Engineer</h2>
       </div>
       <div className="welcome-btn">
-        <NavLink to="/AboutMe" className="btn-txt">
+        <NavLink to="/Projects" className="btn-txt">
           Explore My Work
         </NavLink>
+      </div>
+      <div className="built-scratch">
+        <h4>Built From Scratch with React</h4>
       </div>
       <div className="comp-logo">
         <img data-aos="zoom-in" src="assets/comp.png" alt="computer" />
